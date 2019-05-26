527b6f5699b81a851a56f9323b44a4ff948dbc2f
VictorGil <victorgilsanz@gmail.com>
2019-05-26 22:38:51 +0200

    Displaying tweets and adding CSS code for a list

--- src/app/classes/dateUtil.ts
+++ src/app/classes/dateUtil.ts
@@ -2,19 +2,29 @@ export default class DateUtil {
 
     public static getCurrentTimeString(): string {
         const date: Date = new Date();
-        const result: string = date.toLocaleString() + '.' + date.getMilliseconds();
+        const result: string = date.toLocaleString() + '.' +
+            DateUtil.padWithZeros(date.getMilliseconds());
+
         return result;
     }
 
     public static getTimeStringFromString(time: string): string {
         const millisString: string = time.substr(0, time.length - 4);
         const millisNumber: number = parseInt(millisString, 10);
+
         return DateUtil.getTimeStringFromNumber(millisNumber);
     }
 
     public static getTimeStringFromNumber(time: number): string {
         const date: Date = new Date(time);
-        const result: string = date.toLocaleString() + '.' + date.getMilliseconds();
+        const result: string = date.toLocaleString() + '.' +
+            DateUtil.padWithZeros(date.getMilliseconds());
+
         return result;
+    }
+
+    public static padWithZeros(num: number): string {
+        const numStr = num + '';
+        return numStr.padStart(3, '0');
     }
 }
